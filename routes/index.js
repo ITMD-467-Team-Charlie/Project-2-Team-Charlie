070a8fbd66a02720c5983156b21094cfa43d8126
sharath.2101352 <72086541+sharath2101352@users.noreply.github.com>
2021-04-22 21:30:02 +0530

    FIx Lint issues

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  let usercookie = req.cookies['user'];
- 
-  if (usercookie == null) { //Executes if variable is null OR undefined
-    
-    res.cookie('user', 'test', {
-      maxAge: 86400 * 1000, // 24 hours
-    });
-  }else{
-    // do something else
-  }
-  res.render('home', { title: 'Nutrition Guide : Team Charlie Project' });
+/**
+ * 
+ */
+router.get('/', function (req, res, next) {
+    let usercookie = req.cookies.user;
+
+    if (usercookie == null) { // Executes if variable is null OR undefined
+        res.cookie('user', 'test', {
+            maxAge: 86400 * 1000 // 24 hours
+        });
+    } else {
+        // do something else
+    }
+    res.render('home', { title: 'Nutrition Guide : Team Charlie Project' });
 });
 
 module.exports = router;
