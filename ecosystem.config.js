a1517816ee57f90e11a6502ac62587d5b545429c
sharath.2101352 <72086541+sharath2101352@users.noreply.github.com>
2021-05-08 20:14:42 +0530

    pm2 config fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,8 @@ module.exports = {
   apps : [{
     name: "nutritionguide",
     script: "start ./bin/www",
-    instances: "max",
+    instances: "1",
+	exec_mode: 'cluster_mode',
     env: {
       NODE_ENV: "development",
     },
